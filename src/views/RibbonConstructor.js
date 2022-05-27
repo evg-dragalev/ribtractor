32c47dc2c580e8831f88a2cfc2b26cf62b282504
Eugene Dragalev <eugene.dragalev@expert-soft.by>
2022-05-28 00:20:37 +0300

    Remove unused generated files, setup initial component, update README

---
+++ src/views/RibbonConstructor.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function RibbonConstructor() {
+  return (
+    // select ribbon color component
+    // select font color component
+    // configure ribbon layout component for girls
+    // configure ribbon layout component for boys
+    // specify list of students component
+    // checkout view component
+    <div>This is RibbonConstructor</div>
+  );
+}
+
+export default RibbonConstructor;
